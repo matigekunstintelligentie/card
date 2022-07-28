f950f5a68575cadb128a4361ff1d553bc9c83c84
Matigekunstintelligentie <matigekunstintelligentie@gmail.com>
2022-07-28 20:26:01 +0200

    1465 simultaneous declarations separated by comma

--- c.c
+++ c.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
 #include <math.h>//gcc -o c c.c -lm;./c>c.ppm
-typedef float f;typedef int d;d R=600;d C=1050;
+typedef float f;typedef int d;d R=600,C=1050;
 f z(f x,f o){return fmod(fmod(x,o)+o,o);}
-f o=1/22.;f p=.04;f H=.5;f V=.25;f Z=1.5;
+f o=1/22.,p=.04,H=.5,V=.25,Z=1.5;
 d main(){printf("P6 %i 600 255 ",C
 );f (*q)[C][4];q=malloc(R*144*C);f e=.07;f b=.14;f I[25][6]
 ={{0,-b,b,0,-3,1},{0,-b,p,0,-2,Z},{e,0,0,b,
@@ -23,7 +23,7 @@ p,-b,p,p,5.4,Z},{p,b,-b,p,4.5,1.8},{p,-b,b,p,
 ,0,8.5,1},{0,-b,b,0,9.5,1},{p,b,-b,p,8.5,Z}};f
 t=H;f s=H;d i=0;d S=22;f Y=H;f X=-b;f L=R*C;f 
 k=o;while(i<2*L){if(i==L){k=p;memcpy(&I,&J,
-R);S=25;Y=-.1;}f a=(f)rand()/RAND_MAX;f P=0;for(
+R);S=25;Y=0;}f a=(f)rand()/RAND_MAX;f P=0;for(
 d z=0;z<S;z++){P+=k;if(a<P){f G=t*I[z][0]+s*I[z]
 [1]+I[z][4];s=t*I[z][2]+s*I[z][3]+I[z][5];t=G;
 break;}}d W=(d)(z((1-s*.1+Y),1)*R);d B=(d)(z(t*
