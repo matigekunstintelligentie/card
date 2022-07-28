b0e197f3e206e0984a93e5b51be402cd6d5a164f
Matigekunstintelligentie <matigekunstintelligentie@gmail.com>
2022-07-28 20:19:50 +0200

    1477 contracted function into main

--- c.c
+++ c.c
@@ -3,7 +3,8 @@
 typedef float f;typedef int d;d R=600;d C=1050;
 f z(f x,f o){return fmod(fmod(x,o)+o,o);}
 f o=1/22.;f p=.04;f H=.5;f V=.25;f Z=1.5;
-void c(f (*q)[C][4]){f e=.07;f b=.14;f I[25][6]
+d main(){printf("P6 %i 600 255 ",C
+);f (*q)[C][4];q=malloc(R*144*C);f e=.07;f b=.14;f I[25][6]
 ={{0,-b,b,0,-3,1},{0,-b,p,0,-2,Z},{e,0,0,b,
 -3,2},{e,0,0,b,-3,1},{p,-b,b,p,-1,1},{p,b,-b,p
 ,-1,1.6},{p,0,0,b,-1,1},{b,0,0,b,1,2},{0,-b,
@@ -27,8 +28,7 @@ d z=0;z<S;z++){P+=k;if(a<P){f G=t*I[z][0]+s*I[z]
 [1]+I[z][4];s=t*I[z][2]+s*I[z][3]+I[z][5];t=G;
 break;}}d W=(d)(z((1-s*.1+Y),1)*R);d B=(d)(z(t*
 .1*R/C+H+X,1)*C);q[W][B][0]=1;q[W][B][1]=1;q[W][
-B][2]=1;i++;}}d main(){printf("P6 %i 600 255 ",C
-);f (*q)[C][4];q=malloc(R*144*C);c(q);for(d x=0;
+B][2]=1;i++;}for(d x=0;
 x<R;x++){for(d y=0;y<C;y++){f E=255;printf(
 "%c%c%c",(d)(q[x][y][0]*E),(d)(q[x][y][1]*E),(d)
 (q[x][y][2]*E));}}}
